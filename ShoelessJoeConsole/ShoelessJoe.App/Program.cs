1c69fe593cc0fefd6e6769c50e89d05cfd1825b7
Tyler Claypool <claypooltyler00@gmail.com>
2020-05-29 01:40:09 -0500

    Installed nugget packages

---
+++ ShoelessJoeConsole/ShoelessJoe.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShoelessJoe.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
